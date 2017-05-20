5a7b0bb27d8bb8c7959daf29d1f798b09d99c888
Asyanin Mikhail <creative.doomsday@gmail.com>
2017-05-20 15:54:49 +0300

    dom parser and ozon parser added, links in README have been added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bookstores-parser
+
+Bookstores parser for online shops:
+
+- [Ozon](https://ozon.com)
+
+- [Amazon](https://amazon.com)
+
+- [Ebay](https://www.ebay.com/rpp/books)
---
+++ ozon-stat-shablon.php
@@ -0,0 +1,57 @@
+File Edit Options Buffers Tools PHP C Help                                                                                                                     
+File Edit Options Buffers Tools PHP C Help                                                                                                                     
+<?php
+
+require_once('simple_html_dom.php'); /* Needed for file_get_html */
+
+function get_rating($div) {
+  foreach ($div->find('div') as $subdiv) {
+    $matches = NULL;
+    preg_match('/bStars inline m([0-9]+).*/', $subdiv->class, $matches);
+    if ($matches) {
+      return (int) $matches[1];
+    }
+  }
+}
+
+function parse_bestsellers($html) {
+  foreach ($html->find('div') as $div) {
+    if ($div->class == "bOneTile inline jsUpdateLink mRuble") {
+      $subdivs = $div->find('div');
+      $title = NULL;
+      foreach ($subdivs as $subdiv) {
+        if ($subdiv->class == "eOneTile_ItemName") {
+          $title = $subdiv->innertext;
+        }
+      }
+
+      $price = NULL;
+      foreach ($div->find('span') as $span) {
+        if ($span->class == "eOzonPrice_main") {
+          $price = $span->innertext;
+          break;
+        }
+      }
+
+      $rating = get_rating($div);
+
+      print("Book:\n");
+      print("  Title: " . $title . "\n");
+      print("  Price: " . $price . "\n");
+      print("  Rating: ");
+      for ($idx = 0; $idx < $rating; ++$idx) {
+        print('*');
+      }
+      print("\n");
+      print("  URL:    " . 'https://www.ozon.ru/' . $div->attr['data-href'] . "\n");
+
+    }
+  }
+}
+
+/* $html = file_get_html("https://www.ozon.ru/context/best_books/"); */
+
+$html = file_get_html("test.html");
+
+parse_bestsellers($html);
+
